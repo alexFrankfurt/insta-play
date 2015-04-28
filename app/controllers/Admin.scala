fd7bdea6778daf49e31106ef3efee4fae23342f6
alexFrankfurt <alex_frankfurt@mail.ru>
2015-04-28 20:03:53 +0300

    flexible scopes, java -> scala index page

--- app/controllers/Admin.scala
+++ app/controllers/Admin.scala
@@ -2,23 +2,30 @@ package controllers
 
 import javax.inject.Inject
 
-import models.slick.Users
-import play.api.mvc.{Action, Controller}
-import scala.slick.driver.MySQLDriver.simple._
 import constants.AppConstants
+import models.slick.Users
+import org.jinstagram.entity.users.feed.MediaFeedData
+import play.api.mvc.{Action, Controller}
 
+import scala.collection.{mutable, JavaConversions}
+import scala.slick.driver.MySQLDriver.simple._
 import scala.slick.lifted.TableQuery
 
-class Admin @Inject() (ca: AppConstants, oa: OAuther) extends Controller{
-  import ca._
+class Admin @Inject() (ac: AppConstants, oa: OAuther) extends Controller{
+  import ac._
   import oa._
   def adminPage = Action {
     val users = TableQuery[Users]
     val usersR = DBIP withSession { implicit session =>
       users.run
     }
-    val feed = instagram.getUserLikes(DummyUserId)
-    val uList = feed.getUserList.toString
-    Ok(views.html.homepage.main(usersR.toString))
+
+    val feedTag = JavaConversions.iterableAsScalaIterable(instagram.getRecentMediaTags("like4like", 2).getData)
+    val feed = instagram.getUserLikes(DummyUserId).toString
+    Ok(views.html.homepage.main(feedTag.toString))
+  }
+  
+  def test = Action {
+    Ok(views.html.index(AuthorizationUrl, ScopeLikes))
   }
 }
