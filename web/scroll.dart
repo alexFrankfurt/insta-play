3315e57c0e2616d8ff78f4ed7956bf2ba3c0ad92
alexFrankfurt <alex_frankfurt@mail.ru>
2015-05-17 16:14:43 +0300

    add infinity scroll This implemented with use of dart isolates. Add some other pages load in app common style.

--- web/scroll.dart
+++ web/scroll.dart
@@ -5,45 +5,45 @@ import 'dart:isolate';
 
 final LOAD_MORE = "Load more";
 final LOADED = "loaded";
+final mediaCountH = querySelector("#mediaCount");
+final photosUl = querySelector("#photos");
 
-class DocumentAndSendPort {
-  HtmlDocument doc;
-  SendPort port;
-  DocumentAndSendPort(this.doc, this.port);
-}
-
-appendPhoto(SendPort port, HtmlDocument doc) => (ProgressEvent e) {
+appendPhoto(SendPort port, SendPort mainPort) => (ProgressEvent e) {
   print("New photos loaded!");
   var photos = e.target.responseText;
-  doc.querySelector("#photos").appendHtml(photos);
-  port.send([LOADED, ""]);
+//  querySelector("#photos").appendHtml(photos);
+  port.send(["", mainPort, photos]);
 };
 
-void scrollStatus(DocumentAndSendPort dasp) {
+void scrollStatus(SendPort replyTo) {
   var state = false;
   var port = new ReceivePort();
-  var doc = dasp.doc;
-  var replyTo = dasp.port;
   HttpRequest photoRequest = new HttpRequest();
   replyTo.send(port.sendPort);
   port.listen((msg) {
     print("Isolate got message $msg");
     var data = msg[0];
     SendPort replyTo = msg[1];
-    if (data == "START") {
-      replyTo.send("Yes, I got your message :)");
-      replyTo.send(data);
-    } else if (data == LOAD_MORE) {
+    if (msg.length == 3) {
+      var data = msg[2];
+      replyTo.send([msg, data]);
+    } else {
+      if (data == "START") {
+        replyTo.send(["Yes, I got your message :)"]);
+        replyTo.send([data]);
+      } else if (data == LOAD_MORE) {
         print("Try to load more");
         if (state == false) {
           state = true;
           print("border!");
           photoRequest.open("GET", "/loadphotos");
-          photoRequest.onLoadEnd.listen(appendPhoto(port.sendPort, doc));
+          photoRequest.onLoadEnd.listen(appendPhoto(port.sendPort, replyTo));
           photoRequest.send();
-        }
-    } else if (data == LOADED) {
-      state = false;
+        } else
+          print("Got back pressure");
+      } else if (data == LOADED) {
+        state = false;
+      }
     }
   });
 }
@@ -51,12 +51,21 @@ void scrollStatus(DocumentAndSendPort dasp) {
 void main() {
   print("Isolate test");
   ReceivePort reply = new ReceivePort();
-  Future<Isolate> remote = Isolate.spawn(scrollStatus, new DocumentAndSendPort(document, reply.sendPort));
+  Future<Isolate> remote =  Isolate.spawn(scrollStatus, reply.sendPort);
   remote.then((_) => reply.first).then((SendPort port) {
     reply = new ReceivePort();
     port.send(["START", reply.sendPort]);
     reply.listen((msg) {
-      print("Message received from isolate: $msg");
+      if (msg.length == 1)
+        print("Message received from isolate: $msg");
+      else {
+        var data = msg[1];
+        print("Got important data: ");
+        photosUl.appendHtml(data);
+        mediaCountH.text = "Media count: " + photosUl.childNodes.length.toString();
+        print(data);
+        port.send([LOADED, reply.sendPort]);
+      }
     });
     window.onScroll.listen(loadMore(reply, port));
   });
